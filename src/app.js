33653d7293e60fdce0732a0c137c566a9fcff242
Hitesh Venugopalan <hiteshvkrish@gmail.com>
2026-01-11 22:35:26 +0530

    fix: add document data model and MongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("MongoDB connected"))
+    .catch(err => console.error(err))
 
 app.get('/health',(req,res) => {
     res.json({status:'ok'})
